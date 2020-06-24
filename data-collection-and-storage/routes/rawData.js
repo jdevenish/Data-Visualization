c98d00671257fce4cefbb2f7a806b350f84e62c2
jdevenish <justin.devenish@gmail.com>
2020-06-24 13:36:57 -0400

    same typo, different file

--- data-collection-and-storage/routes/rawData.js
+++ data-collection-and-storage/routes/rawData.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const rawdataController = require("../controllers/rawdataController");
+const rawdataController = require("../controllers/rawData");
 
 
 router.get('/', rawdataController.getData);

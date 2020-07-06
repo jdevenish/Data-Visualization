3e156f8b89701d73967b859277960513bcb4a222
jdevenish <justin.devenish@gmail.com>
2020-07-06 11:22:04 -0400

    Starting data aggregation to return on submit API call

--- data-collection-and-storage/controllers/rawData.js
+++ data-collection-and-storage/controllers/rawData.js
@@ -1,13 +1,55 @@
 const RawData = require("../models/RawData");
+const MetricSchema = require("../models/Metrics");
 
 const addData = (req, res) => {
     // Create raw data entry
     RawData.create(req.body).then(() => {
-        res.status(200).
-            json({
-                status: 200,
-                message: "Data added successfully"
+        RawData.find().then(allData => {
+            let metricObj = new MetricSchema;
+
+            allData.forEach(dataObj => {
+                // Browser
+                switch (dataObj.deviceType) {
+                    case "Firefox":
+                        metricObj.browser.firefox += 1;
+                        break;
+                    case "Chrome":
+                        metricObj.browser.chrome += 1;
+                        break;
+                    case "Safari":
+                        metricObj.browser.safari += 1;
+                        break;
+                    case "IE":
+                        metricObj.browser.ie += 1;
+                        break;
+                    case "":
+                        break;
+                    default:
+                        metricObj.browser.other += 1;
+                }
+
+                // Device Type
+                if(dataObj.screenWidth < 480) {
+                    metricObj.deviceType.mobile += 1;
+                } else if(768 <= dataObj.screenWidth && dataObj.screenWidth < 1024 ){
+                    metricObj.deviceType.tablet += 1;
+                } else if(dataObj.screenWidth >= 1024){
+                    metricObj.deviceType.desktop += 1;
+                }
+
+                // Load Times
+                // Add sort by date
+                metricObj.loadTimes.data.push(dataObj.loadTime);
+
+                // Locale
+                // Define default sort and do that here. (date, city, etc..)
+                metricObj.location.push(dataObj.geolocation);
+            });
+
+            res.status(200)
+                .json(metricObj)
         })
+
     }).catch(err => {
         res.status(200)
             .json({
@@ -34,6 +76,8 @@ const getData = (req, res) => {
     });
 };
 
+// Find and remove all raw data documents.
+// Used to reset the collection for testing purposes.
 const clearData = (req, res) => {
     RawData.find().then(allData => {
         allData.forEach(dataObj => {

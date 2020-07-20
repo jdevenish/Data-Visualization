50494344593e16893cdd0ac098db1c49dacc62d8
jdevenish <justin.devenish@gmail.com>
2020-07-20 14:49:55 -0400

    Stil working on line chart graph

--- graphs-and-charts/src/components/LineChart.js
+++ graphs-and-charts/src/components/LineChart.js
@@ -45,7 +45,7 @@ export default function LineChart({ id, graphData, colorObj, title = ""}) {
     const svg = d3.select(id)
         .attr("preserveAspectRatio", "xMinYMin meet")
         .attr("viewBox", "-"
-            + adj + " - "
+            + adj + " -"
             + adj + " "
             + (dimensions.width + adj * 3) + " "
             + (dimensions.height + adj * 3))
@@ -53,21 +53,89 @@ export default function LineChart({ id, graphData, colorObj, title = ""}) {
         .style("margin", margin)
         .classed("svg-content", true);
 
+
+
     //---------------- DATA --------------------------------------------------//
     const timeConv = d3.timeParse("%d-%b-%Y");
 
-    // const dataset = d3.json(graphData.data);
-    //---------------- SCALES ------------------------------------------------//
-    const xScale = d3.scaleTime().range([0,dimensions.width]);
-    const yScale = d3.scaleLinear().rangeRound([dimensions.height, 0]);
-    
+    if(graphData) {
+        const slices = graphData.data.map((value) => {
+            // console.log(timeConv(new Date(value.date).toString()))
+            return {
+                date: new Date(value.date),
+                measurement: value.time
+            }
+        });
 
-    //---------------- AXES --------------------------------------------------//
-    //---------------- LINES -------------------------------------------------//
-    //------------- 2. DRAWING -----------------------------------------------//
-    //---------------- AXES --------------------------------------------------//
-    //---------------- LINES -------------------------------------------------//
 
+        // const dataset = d3.json(graphData.data);
+        //---------------- SCALES ------------------------------------------------//
+        const xScale = d3.scaleTime().range([0,dimensions.width]);
+        const yScale = d3.scaleLinear().rangeRound([dimensions.height, 0]);
+
+        xScale.domain(d3.extent(graphData.data, function(d){
+            return timeConv(d.date)}));
+
+        yScale.domain([(0), d3.max(slices, function(c) {
+            return d3.max(c.measurement + 4)
+        })
+        ]);
+
+        //---------------- AXES --------------------------------------------------//
+        const yaxis = d3.axisLeft()
+            .ticks(slices.length)
+            .scale(yScale);
+
+        const xaxis = d3.axisBottom()
+            .ticks(d3.timeDay.every(1))
+            .tickFormat(d3.timeFormat('%b %d'))
+            .scale(xScale);
+
+        //---------------- LINES -------------------------------------------------//
+        const line = d3.line()
+            .x(function(d) { return xScale(d.date); })
+            .y(function(d) { return yScale(d.measurement); });
+
+
+        //------------- 2. DRAWING -----------------------------------------------//
+        //---------------- AXES --------------------------------------------------//
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + dimensions.height + ")")
+            .call(xaxis);
+
+        svg.append("g")
+            .attr("class", "axis")
+            .call(yaxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("dy", ".75em")
+            .attr("y", 6)
+            .style("text-anchor", "end")
+            .text("Frequency");
+
+
+        const lines = svg.selectAll("lines")
+            .data(slices)
+            .enter()
+            .append("g");
+
+        lines.append("path")
+            .attr("d", function(d) { return line(d); });
+
+        lines.append("text")
+            .attr("class","serie_label")
+            .datum(function(d,i) {
+                // console.log(`d = ${d}  i = ${i}`)
+                return slices[slices.length - 1]; })
+            .attr("transform", function(d) {
+                // console.log(d)
+                return "translate(" + (xScale(d.date) + 10)
+                    + "," + (yScale(d.measurement) + 5 ) + ")"; })
+            .attr("x", 5)
+
+        //---------------- LINES -------------------------------------------------//
+    }
     return (
         <div className="line-graph-container">
             <div className="pie-graph-svg-container">
